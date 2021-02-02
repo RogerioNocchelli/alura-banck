ca7c63fe70c68e2eb57432b37ce5762fc1c058c0
Rogerio Nocchelli <rogerionocheli48@gmail.com>
2021-02-02 20:33:50 -0300

    Adjust files to step 3

--- app/js/models/Negociacoes.ts
+++ app/js/models/Negociacoes.ts
@@ -1,10 +1,11 @@
 class Negociacoes {
-    constructor() {
-        this._negociacoes = [];
-    }
+    
+    private _negociacoes: Array<Negociacao> = [];
+
     adiciona(negociacao) {
         this._negociacoes.push(negociacao);
     }
+    
     paraArray() {
         return this._negociacoes;
     }
